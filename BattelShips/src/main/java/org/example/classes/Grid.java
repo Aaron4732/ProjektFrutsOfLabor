979163d1e97ceb6b3e9db4b6b10fafe5aaca313a
aaron <aaron-dario.fries@stud.fh-campuswien.ac.at>
2022-12-28 13:14:07 +0100

    Create the grids

--- BattelShips/src/main/java/org/example/classes/Grid.java
+++ BattelShips/src/main/java/org/example/classes/Grid.java
@@ -1,15 +1,24 @@
 package org.example.classes;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Grid {
     int gridHight = 8;
     int gridBroad = gridHight;
 
+    //IntStream.rangeClosed(1, gridHight) =
+    //.boxed() =
+    //.map(i -> Collections.nCopies(gridBroad, 0)) =
+    //.collect(Collectors.toList()) =
+    List<List<Integer>> shipGrid = IntStream.rangeClosed(1, gridHight).boxed().map(i -> Collections.nCopies(gridBroad, 0)).collect(Collectors.toList());
+    List<List<Integer>> shotGrid = IntStream.rangeClosed(1, gridHight).boxed().map(i -> Collections.nCopies(gridBroad, 0)).collect(Collectors.toList());
+    // https://www.techiedelight.com/initialize-list-of-lists-java/
     public Grid() {
-    }
 
-    public Grid(int gridHight, int gridBroad) {
-        this.gridHight = gridHight;
-        this.gridBroad = gridBroad;
     }
 
     public void setGridHight(int gridHight) {
@@ -21,7 +30,8 @@ public class Grid {
     }
 
     public void printGrid() {
-        System.out.println("Test");
+        shipGrid.forEach(System.out::println);
+        shotGrid.forEach(System.out::println);
     }
 
 }
